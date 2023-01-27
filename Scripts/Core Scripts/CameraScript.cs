7a8b822ca09e6180edfac99b92719ca62956c200
mehmeterk-computethis <34861665+mehmeterk-computethis@users.noreply.github.com>
2023-01-27 23:38:15 +0000

    Update CameraScript.cs

--- Scripts/Core Scripts/CameraScript.cs
+++ Scripts/Core Scripts/CameraScript.cs
@@ -16,10 +16,8 @@ public class CameraScript : MonoBehaviour
     void Start()
     {
         target = GameObject.FindWithTag("Player").transform;
-        Debug.Log("HERE "+ target);
         thisCam = GetComponent<Camera>();
         thisListener = GetComponent<AudioListener>();
-        Debug.Log(gameObject.name + " Cam is " + thisCam);
         thisCam.enabled = active;
         gameObject.tag = active ? "ActiveCamera" : "Camera";
 
@@ -39,17 +37,4 @@ public class CameraScript : MonoBehaviour
 
 
     }
-    //BELOW NOT IMPLEMENTED//
-   public void updatePlayerCam() {
-        StartCoroutine(UpdateActiveCamera(target.gameObject));
-    }
-    IEnumerator UpdateActiveCamera(GameObject player)
-    { //No need to get the rigidbody controller //
-        Rigidbody_Controller playerController = player.GetComponent<Rigidbody_Controller>();
-        playerController.activeCamera = this.gameObject;
-        yield return new WaitForSeconds(1);
-        playerController.pointerCamera = transform.GetChild(0).GetComponent<Camera>();
-
-    }
-    //Function: updatePlayerCam and UpdateActiveCamera are only necessary if the player is using rotType.screenPos (rotation type  "Looking at cursor")
 }
